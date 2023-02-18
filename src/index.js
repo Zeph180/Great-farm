cc248383cf99d1fbca146be2b8c28a13a358b8ed
Kigozi Joseph  Suubi <kigozijosephsuubi1@gmail.com>
2023-02-18 10:40:29 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
