701b4d143ea91bfa5ad668b0eadb9ac62805835b
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-02-23 16:03:14 +0300

    Add ham interaction

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 // import _ from 'lodash';
+import { hamburgerEvents, closeEvent } from './js/hamburger';
 import './style.css';
 
-const hamburger = document.querySelector('close-btn');
-
-hamburger.addEventListener('click', console.log('yooo'));
-
-function component() {
-  console.log('dddd');
-}
-
-component();
+hamburgerEvents();
+closeEvent();
