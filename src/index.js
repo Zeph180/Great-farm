75554442d8cf54fd4aa84df68c8749b75996d6e1
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-02-24 16:40:22 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 // import _ from 'lodash';
-import { aboutSecImg, servicesBtn, servicesSec, whyUs, whyUsBtn } from './domElements';
-import createCard from './js/card';
-import { hamburgerEvents, closeEvent } from './js/hamburger';
+import {
+  servicesBtn, servicesSec, whyUs, whyUsBtn,
+} from './domElements.js';
+import createCard from './js/card.js';
+import { hamburgerEvents, closeEvent } from './js/hamburger.js';
 import './style.css';
-import { reasons, services } from './js/services';
+import { reasons, services } from './js/services.js';
 
 const serviceStyle = {
   container: 'card',
--- src/js/card.js
+++ src/js/card.js
@@ -1,21 +1,3 @@
-import { servicesBtn } from '../domElements.js';
-
-// const createCard = (cards, target) => {
-//   cards.forEach((card) => {
-//     const cardTemp = document.createElement('article');
-//     cardTemp.classList.add('card');
-//     cardTemp.innerHTML = `
-//       <div class="icon-cont"><img class="service-img" src=${card.icon} alt=""> </div>
-//       <h4 class="card-title">${card.title}</h4>
-//       <p class="card-desc">
-//         ${card.desc}
-//       </p>
-//     `;
-//     console.log(card.title);
-//     target.insertBefore(cardTemp, servicesBtn);
-//   });
-// };
-
 const createCard = (cards, target, styles, btn) => {
   cards.forEach((card) => {
     const cardTemp = document.createElement('article');
